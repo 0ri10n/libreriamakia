bd65ee369c0f8d147c7dbdcde8934fbcf234aa19
Isos <al07073224@gmail.com>
2026-02-14 18:39:53 -0600

    estructura inicial

--- backend/server.js
+++ backend/server.js
@@ -158,7 +158,7 @@ app.get('/api/loans/all', proteger, async (req, res) => {
 // --- RUTAS DE PRÉSTAMOS (USUARIO) ---
 app.get('/api/loans', proteger, async (req, res) => {
     try {
-
+        // Busca préstamos donde el campo 'user' coincida con el ID del token
         const loans = await Loan.find({ user: req.user.id }).populate('book');
         res.json(loans);
     } catch (err) {
